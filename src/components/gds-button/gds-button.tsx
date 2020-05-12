796f669003007ebb6236bc24ff077c58628c60f1
Oskar Schöldström <m@oxy.fi>
2020-05-12 19:44:30 -0300

    use sass

--- src/components/gds-button/gds-button.tsx
+++ src/components/gds-button/gds-button.tsx
@@ -2,7 +2,7 @@ import { Component, Host, h } from '@stencil/core';
 
 @Component({
   tag: 'gds-button',
-  styleUrl: 'gds-button.css',
+  styleUrl: 'gds-button.scss',
   shadow: true,
 })
 export class GdsButton {
