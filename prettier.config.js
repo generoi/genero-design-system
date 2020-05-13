0729ae5fa6b1816ec074c030765c84ad0b88eb3a
Oskar Schöldström <m@oxy.fi>
2020-05-13 07:54:19 -0300

    adjust prettier config #3

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   tabWidth: 2,
-  printWidth: 80,
+  printWidth: 120,
   singleQuote: true,
   useTabs: false,
+  semi: false,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
   trailingComma: 'all',
-};
+}
