c99b4a20b711d1c3f92559a12da33d53472578ca
Taro Morimoto <taro.morimoto@a-lehdet.fi>
2020-06-12 16:33:25 +0300

    Remove seemingly unused storybook preview iframeHeight parameter

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,5 @@
 /* global window */
 
-import {
-  addParameters,
-} from '@storybook/web-components';
-
 import { defineCustomElements } from "../dist/esm/loader.mjs"
 defineCustomElements()
-
-addParameters({
-  docs: {
-    iframeHeight: '200px',
-  },
-});
 
