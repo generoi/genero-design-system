fe8f9c9604e649cf15550fa2373acacc109af72d
Taro Morimoto <taro.morimoto@a-lehdet.fi>
2020-06-18 15:16:08 +0300

    feat: basic version of gds-media-card
    Still needs:
    
    - gds-text component for description
    - optimized gds-image component
    - superimposed images
    - styles for mobile

--- src/components/gds-media-card/gds-media-card.tsx
+++ src/components/gds-media-card/gds-media-card.tsx
@@ -7,17 +7,61 @@ import { Component, h, Prop } from '@stencil/core'
 })
 export class GdsMediaCard {
   /**
-   *
+   * If defined, the card will be a link.
    */
-  @Prop() heading: string
+  @Prop() href: string
+  /**
+   * Link open target.
+   */
+  @Prop() target: string
+  /**
+   * Title for the card (note: title is a reserved word).
+   */
+  @Prop() headline: string
+  /**
+   * Image url.
+   */
+  @Prop() imageUrl: string
+  /**
+   * Superimposed image url.
+   * TODO: implement this.
+   */
+  @Prop() superimposedImageUrl: string
+  /**
+   * Superimposed image fit.
+   * TODO: implement this.
+   */
+  @Prop() superimposedImageFit: string
+  /**
+   * TODO: implement this.
+   */
+  @Prop() description: string
 
   render() {
-    return (
-      <div>
-        <div>Picture here</div>
-        <div>{this.heading}</div>
-        <div>Labels here</div>
+    console.log('img', this.imageUrl)
+    console.log('headline', this.headline)
+    // Main content
+    const card = (
+      <div class="card">
+        {/* TODO: Implement a gds-image component that implements <picture> and srcset etc. */}
+        <img src={this.imageUrl} class="image" />
+        <div class="headline">
+          <gds-heading size="s">{this.headline}</gds-heading>
+        </div>
+        <div class="tags">
+          <slot></slot>
+        </div>
       </div>
+    )
+
+    // Render without a link
+    if (!this.href) return card
+
+    // Render with a link
+    return (
+      <gds-link href={this.href} target={this.target}>
+        {card}
+      </gds-link>
     )
   }
 }
