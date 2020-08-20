68aa8cb341ac9b3f4bf268a991520e193c7d6b11
Taro <taro.morimoto@gmail.com>
2020-08-20 14:45:35 +0300

    feat(gds-card): add customization via css variables

--- src/components/gds-card/gds-card.stories.js
+++ src/components/gds-card/gds-card.stories.js
@@ -18,4 +18,43 @@ export const Card = () => html`
       With padding
     </gds-card>
   </p>
+  <style>
+    .variation-1 {
+      padding: 40px;
+      --card-border: 1px solid black;
+    }
+    .variation-2 {
+      padding: 40px;
+      --card-background-color: transparent;
+      --card-box-shadow: none;
+    }
+    .variation-3 {
+      padding: 40px;
+      --card-background-color: transparent;
+    }
+    .variation-4 {
+      padding: 40px;
+      --card-border-radius: 0;
+    }
+  </style>
+  <p>
+    <gds-card class="variation-1">
+      With border.
+    </gds-card>
+  </p>
+  <p>
+    <gds-card class="variation-2">
+      Without background color and box shadow.
+    </gds-card>
+  </p>
+  <p>
+    <gds-card class="variation-3">
+      Without background color.
+    </gds-card>
+  </p>
+  <p>
+    <gds-card class="variation-4">
+      With sharp corners.
+    </gds-card>
+  </p>
 `
