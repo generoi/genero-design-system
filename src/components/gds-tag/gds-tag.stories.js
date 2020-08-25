73571b13e641b35403eac4c39ef705425956a33c
Taro <taro.morimoto@gmail.com>
2020-08-25 10:20:34 +0300

    feat(gds-tag): add border and padding variables

--- src/components/gds-tag/gds-tag.stories.js
+++ src/components/gds-tag/gds-tag.stories.js
@@ -13,8 +13,16 @@ export const Tag = () => html`
       --tag-background-color: transparent;
       --tag-border-color: red;
     }
+    .sharp {
+      --tag-color: black;
+      --tag-background-color: transparent;
+      --tag-border: 4px solid black;
+      --tag-border-radius: 0;
+      --tag-padding: 2px 10px 4px 10px
+    }
   </style>
   <gds-tag>Label for labels and tags</gds-tag>
   <gds-tag href="https://www.genero.fi" target="_blank">Label with a link</gds-tag>
   <gds-tag class="override">Label can override colors and borders</gds-tag>
+  <gds-tag class="sharp">Sharp borders</gds-tag>
 `
