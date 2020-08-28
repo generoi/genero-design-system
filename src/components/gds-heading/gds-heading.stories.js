8e553141c91bfa2d330853a34fafe6d9ede37704
Oskar Schöldström <m@oxy.fi>
2020-08-28 14:51:51 -0300

    feat(heading): add xs size

--- src/components/gds-heading/gds-heading.stories.js
+++ src/components/gds-heading/gds-heading.stories.js
@@ -12,5 +12,6 @@ export const Heading = () => html`
   <gds-heading size="l">L Heading</gds-heading>
   <gds-heading size="m">M Heading</gds-heading>
   <gds-heading size="s">S Heading</gds-heading>
+  <gds-heading size="xs">XS Heading</gds-heading>
   <gds-heading size="l" as="h3">L Heading rendered as h3</gds-heading>
 `
