4354865390434f9c3c99e4aaf6434d22ca9c5b59
Oskar Schöldström <public@oxy.fi>
2021-03-26 23:39:43 -0300

    chore(repo): use storybook/preset-scss instead of custom solution

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,22 +9,9 @@ module.exports = {
         backgrounds: false,
       }
     },
+    '@storybook/preset-scss',
     '@storybook/addon-controls',
     '@panosvoudouris/addon-versions',
     '@storybook/addon-a11y',
   ],
-
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../')
-    })
-
-    return config;
-  },
 }
